664df55ff8f1b263af70f524c9a7f8e7a8623f24
Doyeong Kim <doyeong.kim@cern.ch>
2018-10-26 12:24:58 -0500

    change: tt script such that they will work witm quick runs script

--- scripts_FSA_v1/recoilZ_mc.sh
+++ scripts_FSA_v1/recoilZ_mc.sh
@@ -1,14 +1,14 @@
-python prepareScripts.py DY0_1 /hdfs/store/user/caillol/SMHTT_mc_feb13/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_v6_ext1-v2 /hdfs/store/user/doyeong/SMHTT_CONDOR/tautau/myskims/Aug30/ES1_W0
-python prepareScripts.py DY0_2 /hdfs/store/user/caillol/SMHTT_mc_feb13/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_v6_ext2-v1 /hdfs/store/user/doyeong/SMHTT_CONDOR/tautau/myskims/Aug30/ES1_W0
-python prepareScripts.py DY1 /hdfs/store/user/caillol/SMHTT_mc_feb13/DY1JetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_v6-v1 /hdfs/store/user/doyeong/SMHTT_CONDOR/tautau/myskims/Aug30/ES1_W0
-python prepareScripts.py DY2 /hdfs/store/user/caillol/SMHTT_mc_feb13/DY2JetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_v6-v1 /hdfs/store/user/doyeong/SMHTT_CONDOR/tautau/myskims/Aug30/ES1_W0
-python prepareScripts.py DY3 /hdfs/store/user/caillol/SMHTT_mc_feb13/DY3JetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_v6-v1 /hdfs/store/user/doyeong/SMHTT_CONDOR/tautau/myskims/Aug30/ES1_W0
-python prepareScripts.py DY4 /hdfs/store/user/caillol/SMHTT_mc_feb13/DY4JetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_v6-v1 /hdfs/store/user/doyeong/SMHTT_CONDOR/tautau/myskims/Aug30/ES1_W0
+python prepareScripts.py DY0_1 /hdfs/store/user/caillol/SMHTT_mc_feb13/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_v6_ext1-v2 ${1} mc Z
+python prepareScripts.py DY0_2 /hdfs/store/user/caillol/SMHTT_mc_feb13/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_v6_ext2-v1 ${1} mc Z
+python prepareScripts.py DY1 /hdfs/store/user/caillol/SMHTT_mc_feb13/DY1JetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_v6-v1 ${1} mc Z
+python prepareScripts.py DY2 /hdfs/store/user/caillol/SMHTT_mc_feb13/DY2JetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_v6-v1 ${1} mc Z
+python prepareScripts.py DY3 /hdfs/store/user/caillol/SMHTT_mc_feb13/DY3JetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_v6-v1 ${1} mc Z
+python prepareScripts.py DY4 /hdfs/store/user/caillol/SMHTT_mc_feb13/DY4JetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_v6-v1 ${1} mc Z
 
-python prepareScripts.py ggHtoTauTau125 /hdfs/store/user/truggles/SMHTT_signals_may30/GluGluHToTauTau_M125_13TeV_powheg_pythia8_v6-v1 /hdfs/store/user/doyeong/SMHTT_CONDOR/tautau/myskims/Aug30/ES1_W0
-python prepareScripts.py ggH_WW125 /hdfs/store/user/truggles/SMHTT_signals_may30/GluGluHToWWTo2L2Nu_M125_13TeV_powheg_pythia8_v6-v1 /hdfs/store/user/doyeong/SMHTT_CONDOR/tautau/myskims/Aug30/ES1_W0
-python prepareScripts.py VBFHtoTauTau125 /hdfs/store/user/truggles/SMHTT_signals_may30/VBFHToTauTau_M125_13TeV_powheg_pythia8_v6-v1 /hdfs/store/user/doyeong/SMHTT_CONDOR/tautau/myskims/Aug30/ES1_W0
-python prepareScripts.py VBF_WW125 /hdfs/store/user/truggles/SMHTT_signals_may30/VBFHToWWTo2L2Nu_M125_13TeV_powheg_pythia8_v6-v1 /hdfs/store/user/doyeong/SMHTT_CONDOR/tautau/myskims/Aug30/ES1_W0
+python prepareScripts.py ggHtoTauTau125 /hdfs/store/user/truggles/SMHTT_signals_may30/GluGluHToTauTau_M125_13TeV_powheg_pythia8_v6-v1 ${1} mc Z
+python prepareScripts.py ggH_WW125 /hdfs/store/user/truggles/SMHTT_signals_may30/GluGluHToWWTo2L2Nu_M125_13TeV_powheg_pythia8_v6-v1 ${1} mc Z
+python prepareScripts.py VBFHtoTauTau125 /hdfs/store/user/truggles/SMHTT_signals_may30/VBFHToTauTau_M125_13TeV_powheg_pythia8_v6-v1 ${1} mc Z
+python prepareScripts.py VBF_WW125 /hdfs/store/user/truggles/SMHTT_signals_may30/VBFHToWWTo2L2Nu_M125_13TeV_powheg_pythia8_v6-v1 ${1} mc Z
 
 source submit_DY0_1.sh
 source submit_DY0_2.sh
