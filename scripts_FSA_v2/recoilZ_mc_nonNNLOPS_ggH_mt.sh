fbf1e9241b557ddcd1a275302531031b7309163c
Doyeong Kim <doyeong.kim@cern.ch>
2018-11-06 07:55:54 -0600

    fix: ggH sample location for lepton+tau channel

--- scripts_FSA_v2/recoilZ_mc_nonNNLOPS_ggH_mt.sh
+++ scripts_FSA_v2/recoilZ_mc_nonNNLOPS_ggH_mt.sh
@@ -1,5 +1,9 @@
-echo python prepareScripts.py ggHtoTauTau125 /hdfs/store/user/truggles/SMHTT_signals_may30/GluGluHToTauTau_M125_13TeV_powheg_pythia8_v6-v1 ${1} mc Z
-python prepareScripts.py ggHtoTauTau125 /hdfs/store/user/truggles/SMHTT_signals_may30/GluGluHToTauTau_M125_13TeV_powheg_pythia8_v6-v1 ${1} mc Z
+#echo python prepareScripts.py ggHtoTauTau125 /hdfs/store/user/truggles/SMHTT_signals_may30/GluGluHToTauTau_M125_13TeV_powheg_pythia8_v6-v1 ${1} mc Z
+#python prepareScripts.py ggHtoTauTau125 /hdfs/store/user/truggles/SMHTT_signals_may30/GluGluHToTauTau_M125_13TeV_powheg_pythia8_v6-v1 ${1} mc Z
+
+
+echo python prepareScripts.py ggHtoTauTau125 /hdfs/store/user/truggles/SM-HTT_HTXS_ggH_aug31_v1/GluGluHToTauTau_M125_13TeV_powheg_pythia8_v6-v1 ${1} mc Z
+python prepareScripts.py ggHtoTauTau125 /hdfs/store/user/truggles/SM-HTT_HTXS_ggH_aug31_v1/GluGluHToTauTau_M125_13TeV_powheg_pythia8_v6-v1 ${1} mc Z
 
 source submit_ggHtoTauTau125.sh
 
