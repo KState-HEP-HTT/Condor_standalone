6f36a19b6dd37afe5bc52377001dab9edfe99c85
Doyeong Kim <doyeong.kim@cern.ch>
2018-11-15 08:34:08 -0600

    fix: typo

--- scripts_FSA_v2017/recoilZ_mc.sh
+++ scripts_FSA_v2017/recoilZ_mc.sh
@@ -5,7 +5,7 @@ python prepareScripts.py DY1_2 /hdfs/store/user/tmitchel/FSA2017_MC_v1/DY1JetsTo
 python prepareScripts.py DY2_1 /hdfs/store/user/ymaravin/MC_2017/DY2JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_v14-v1 ${1} mc Z
 python prepareScripts.py DY2_2 /hdfs/store/user/ymaravin/MC_2017/DY2JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_v14_ext1-v1${1} mc Z
 python prepareScripts.py DY3_1 /hdfs/store/user/ymaravin/MC_2017/DY3JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_v14-v1 ${1} mc Z
-python prepareScripts.py DY3_2 /hdfs/store/user/ymaravin/MC_2017/DY3JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_v14_ext1-v1${1} mc Z
+python prepareScripts.py DY3_2 /hdfs/store/user/ymaravin/MC_2017/DY3JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_v14_ext1-v1 ${1} mc Z
 python prepareScripts.py DY4 /hdfs/store/user/ymaravin/MC_2017/DY4JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_v2_94X_mc2017_realistic ${1} mc Z
 
 python prepareScripts.py ggHtoTauTau125_1 /hdfs/store/user/ymaravin/MC_2017/GluGluHToTauTau_M125_13TeV_powheg_pythia8_v14-v1 ${1} mc Z
